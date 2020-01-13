f0d53f78d5e26be4a257b2186f8bb8dad1b16cbc
rnlioilm <rnlioilm@gmail.com>
2020-01-13 15:32:19 +0900

    first commit

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    app.logger.info("Request body: " + body) #
 
     try:
         handler.handle(body, signature)
